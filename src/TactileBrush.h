e14bf63d1cf1572b740dad676181afb6126d5a56
Quentin Duchemin <quentinduchemin@tuta.io>
2019-06-25 18:06:01 +0200

    Add computation of virtual actuators from a stroke definition

--- src/TactileBrush.h
+++ src/TactileBrush.h
@@ -2,15 +2,30 @@
 #define _TACTILE_BRUSH_H
 
 #include <utility>
+#include <set>
 #include <vector>
+#include <cmath>
+#include <stdexcept>
+#include <algorithm>
+#include <iostream>
+
+const float EPSILON = 0.001f;
+
+/**
+ * @struct Coord
+ * @brief A pair of float, representing 2D coordinates in the actuators grid
+ * Coordinates are intented to be in centimeters (so they already take into account
+ * distance between actuators)
+ */
+typedef std::pair<float, float> Coord;
 
 /**
  * @struct Stroke
  * @brief Describes a straight-line stroke (start, end, duration)
  */
 struct Stroke {
-  std::pair<float, float> start;
-  std::pair<float, float> end;
+  Coord start;
+  Coord end;
   float duration;
 };
 
@@ -34,7 +49,10 @@ public:
    * @param columns  Number of columns of the grid
    * @param distance Distance between two actuators
    */
-  TactileBrush(unsigned int lines, unsigned int columns, float distance);
+  TactileBrush(unsigned int lines, unsigned int columns, float distance) : lines(lines - 1), columns(columns - 1), interDist(distance) {
+    minCoord = Coord(0, 0);
+    maxCoord = Coord(columns * interDist, lines * interDist);
+  }
 
   /**
    * @brief Compute the activation steps for a given stroke
@@ -45,14 +63,22 @@ public:
    * @param  s Desired stroke
    * @return   Vector of ActuatorStep to get the desired haptic stroke illustion
    */
-  std::vector<ActuatorStep> computeStroke(Stroke s);
+  std::set<ActuatorStep> computeStroke(const Stroke& s);
 private:
-  // Lines
-  unsigned int n;
-  // Columns
-  unsigned int m;
-  // Distance between actuators
-  float l;
+  unsigned int lines;
+  unsigned int columns;
+  float interDist;
+
+  Coord minCoord;
+  Coord maxCoord;
+
+  std::vector<Coord> computeVirtualPoints(const Stroke& s);
+
+  bool isPointOnSegment(const Coord& point, const Coord& start, const Coord& end);
+  bool isPointWithinGrid(const Coord& point);
+  inline void printCoord(const Coord& c) {
+    std::cout << "(" << c.first << "," << c.second << ")" << std::endl;
+  }
 };
 
 #endif
